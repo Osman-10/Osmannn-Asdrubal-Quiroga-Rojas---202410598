9ab58ba0f9ad3e1034724394ea9a2b8bd5d58370
Osman-10 <osman.quiroga@uptc.edu.co>
2025-10-16 12:08:32 -0500

    Update ejercicio2.cpp

--- TALLER CLASE/ejercicio2.cpp
+++ TALLER CLASE/ejercicio2.cpp
@@ -1,1 +1,48 @@
+// ing. me toco subirlo aca despues de tiempo como habiamos acoradado!!!!
+
+
+#include <iostream>
+using namespace std;
+
+bool esPositi(char radio) {
+    if (radio == '-') {
+        return true;
+    } else {
+        return false;
+    }
+}
+bool esNegati(char radio) {
+    if (radio == '.') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void validSignals(string senales) {
+    int positivas = 0;
+    int negativas = 0;
+
+    for (int i = 0; i < senales.size(); i++) 
+    {
+        if (esPositi(senales[i])) {
+            positivas++;
+        } 
+        else if (esNegati(senales[i])) {
+            negativas++;
+        }
+    }
+
+     cout << "Senales positivas (-): " << positivas << endl;
+    cout << "Senales negativas (.): " << negativas << endl;
+}
+
+int main() {
+    string signals;
+    cout << "Ingrese  senales (- o .): ";
+    cin >> signals;
+    validSignals(signals);
+
+    return 0;
+}
 
